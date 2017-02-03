f76ce275a971206f0c920b5c64b0f8b41312884b
mavpokit <zoneraider@inbox.ru>
2017-02-03 17:54:40 +0200

    SMS manager class with permissions

--- app/src/main/java/com/mavpokit/rseriesalarm/control/ControlActivity.java
+++ app/src/main/java/com/mavpokit/rseriesalarm/control/ControlActivity.java
@@ -1,6 +1,7 @@
 package com.mavpokit.rseriesalarm.control;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -90,20 +91,19 @@ public class ControlActivity extends AppCompatActivity {
     @OnClick(R.id.arm_button)
     void armButtonClick() {
         smsMessage = alarmObject.getCode() + "AA";
-        sendSms();
+        MySmsManager.sendSms(this, alarmObject.getNumber(),smsMessage);
     }
 
     @OnClick(R.id.disarm_button)
     void disarmButtonClick() {
         smsMessage = alarmObject.getCode() + "BB";
-        sendSms();
+        MySmsManager.sendSms(this, alarmObject.getNumber(),smsMessage);
     }
-
 
     @OnClick(R.id.request_status_button)
     void requstStatusButtonClick() {
         smsMessage = alarmObject.getCode() + "EE";
-        sendSms();
+        MySmsManager.sendSms(this, alarmObject.getNumber(),smsMessage);
     }
 
     @OnClick(R.id.call_button)
@@ -134,103 +134,16 @@ public class ControlActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void sendSms() {
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
-                == PackageManager.PERMISSION_GRANTED) {
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(alarmObject.getNumber(), null, smsMessage, null, null);
-            Toast.makeText(this, "sending control SMS to device...", Toast.LENGTH_SHORT).show();
-        } else {
-            requestPermissionWithRationale();
-        }
-    }
-
-    /**
-     * handles 3 cases:
-     * <p> - first request after install</p>
-     * <p> - request after permission was once denied</p>
-     * <p> - if user set newer_ask_again</p>
-     */
-    public void requestPermissionWithRationale() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.SEND_SMS)) {
-            //permission was once denied
-            Snackbar sb = Snackbar.make(findViewById(R.id.root_view),
-                    R.string.open_sms_permisssion_explanation,
-                    Snackbar.LENGTH_INDEFINITE)
-                    .setAction(R.string.ok, v -> requestSendSMS());
-            showSnackBar(sb);
-
-            SharedPreferences settings = getSharedPreferences(MY_SHARED_PREFS, 0);
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putBoolean(NEVER_ASK_AGAIN, true);
-            editor.commit();
-
-
-        } else {
-            //if user set newer_ask_again, open Settings to grant permission
-            SharedPreferences settings = getSharedPreferences(MY_SHARED_PREFS, 0);
-            boolean newer_ask_again = settings.getBoolean(NEVER_ASK_AGAIN, false);
-
-            if (newer_ask_again) {
-                Snackbar sb = Snackbar.make(findViewById(R.id.control_activity),
-                        R.string.open_settings_explanation,
-                        Snackbar.LENGTH_LONG)
-                        .setAction(R.string.open, v -> openSettings());
-                showSnackBar(sb);
-
-            } else
-                //first request after install
-                requestSendSMS();
-        }
-    }
-
-    /**
-     * Increases lines in snackbar to 4 (default is 2) and shows it
-     *
-     * @param sb
-     */
-    private void showSnackBar(Snackbar sb) {
-        View sbView = sb.getView();
-        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
-        textView.setMaxLines(4);
-        sb.show();
-    }
-
-    private void requestSendSMS() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SEND_SMS_REQUSET_CODE);
-    }
-
-    private void openSettings() {
-        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                Uri.parse("package:" + getPackageName()));
-        startActivity(appSettingsIntent);
-        //startActivityForResult(appSettingsIntent, SEND_SMS_REQUSET_CODE_FROM_SETTINGS);
-
+    @Override
+    protected void onDestroy() {
+        MySmsManager.clearActivityReference();
+        super.onDestroy();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case SEND_SMS_REQUSET_CODE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    sendSms();
-                } else {
-                    Snackbar sb = Snackbar.make(findViewById(R.id.control_activity),
-                            R.string.sms_denied,
-                            Snackbar.LENGTH_INDEFINITE)
-                            .setAction(R.string.ok, v -> {
-                            });
-                    showSnackBar(sb);
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
+        MySmsManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
     }
 }
